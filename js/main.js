9b87bedea12987e9ba44fcc7456707f160b08669
wagner-sousa <wagner.fernandes.sousa@outlook.com>
2023-08-28 20:33:07 +0000

    * Ajutes gerais para trucar o uso do bower por npm * Ajustes para montar um mix de arquivos css e js

---
+++ js/main.js
@@ -0,0 +1,5 @@
+import 'node_modules/jquery/dist/jquery.min.js';
+import 'node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.8.1/baguetteBox.min.js';
+import '../node_modules/jquery-easing/jquery.easing.1.3.js';
+import './resume.js';
