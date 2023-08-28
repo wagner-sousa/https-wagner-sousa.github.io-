773c242fc95cb78e5006d90f56209d8696c24955
wagner-sousa <wagner.fernandes.sousa@outlook.com>
2023-08-28 20:59:05 -0300

    Ajuste em javascript

--- js/_mixins.js
+++ js/_mixins.js
@@ -1,4 +1,4 @@
-import 'jquery/dist/jquery.min.js';
-import 'bootstrap/dist/js/bootstrap.bundle.js';
-import 'jquery-easing/jquery.easing.1.3.js';
+import '../node_modules/jquery/dist/jquery.js';
+import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
+import '../node_modules/jquery-easing/jquery.easing.1.3.jss';
 import './resume.js';
