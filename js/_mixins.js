1f655f910e968db982121590e5e650379ed0b1fb
wagner-sousa <wagner.fernandes.sousa@outlook.com>
2023-08-28 21:04:57 -0300

    Correção ao imoprtar o jquery-easing

--- js/_mixins.js
+++ js/_mixins.js
@@ -1,4 +1,4 @@
 import '../node_modules/jquery/dist/jquery.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.js';
-import '../node_modules/jquery-easing/jquery.easing.1.3.jss';
+import '../node_modules/jquery-easing/jquery.easing.1.3.js';
 import './resume.js';
